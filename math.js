66a5b833c3cc3f04ed1782b982a67b65383ec768
YURIY <yury.myworkmail@gmail.com>
2022-08-31 12:22:01 +0300

    Chapter 1: Start Here ended successfylly

---
+++ math.js
@@ -0,0 +1,12 @@
+const add = ( a, b ) => a + b;
+const subtract = ( a, b ) => a - b;
+const multiply = ( a, b ) => a * b;
+const devide = ( a, b ) => a / b;
+
+module.exports = {add, subtract, multiply, devide}
+
+
+// export.add = ( a, b ) => a + b;              // can work this way too!
+// export.subtract = ( a, b ) => a - b;
+// export.multiply = ( a, b ) => a * b;
+// export.devide = ( a, b ) => a / b;
